331eeb55a9d37528495a7a4dc90f7a9a00e6562b
nizar-art <147240244+nizar-art@users.noreply.github.com>
2024-05-20 17:40:11 +0700

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class categoryController extends Controller
 {
-    public function store(Request $request)
+    public function create(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:255',
@@ -20,17 +20,14 @@ class categoryController extends Controller
 
         $payload = $validator->validated();
         Category::create([
-            'category_name' => $payload['category_name'],
-            'category_type' => $payload['category_type'],
-            'category_price' => $payload['category_price'],
-            'expired_at' => $payload['expired_at']
+            'name' => $payload['name'],
         ]);
 
         return response()->json([
             'message' => 'Data berhasil disimpan'
         ])->setStatusCode(201);
     }
-    function showAll(){
+    public function read(){
         $category = Category::all();
         return response()->json([
             'msg' => 'Data Produk Keseluruhan',
@@ -38,39 +35,12 @@ class categoryController extends Controller
         ],200);
 
     }
-
-    function showById($id){
-        $category = Category::where('id', $id)->first();
-        if ($category) {
-            return response()->json([
-                'msg' => 'Data Produk Dengan ID: '.$id,
-                'data' => $category
-            ],200);
-        }
-        return response()->json([
-           'msg' => 'Data Produk dengan ID: '.$id.' Tidak Ditemukan'
-        ],404);
-    }
-    function showByName($category_name){
-        $category = Category::where('category_name', 'LIKE','%'.$category_name.'%')->get();
-        if ($category->count() > 0 ) {
-            return response()->json([
-                'msg' => 'Data Produk Dengan Nama Yang Mirip: '.$category_name,
-                'data' => $category
-            ],200);
-        }
-        return response()->json([
-           'msg' => 'Data Produk dengan Nama Yang Mirip: '.$category_name.' Tidak Ditemukan'
-        ],404);
-    }
     
     public function update(Request $request, $id)
     {
         $validator = Validator::make($request->all(), [
-            'category_name' => 'required|max:50',
-            'description' => 'required',
-            'price' => 'required|numeric',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            'name' => 'required|max:255',
+ 
         ]);
 
         if ($validator->fails()) {
@@ -103,10 +73,4 @@ class categoryController extends Controller
         ],400);
     }
 
-    public function restore ($id) {
-        $category = Category::onlyTrashed()->where ($id);
-        $category ->restore();
-
-        return response()->json("Data dengan id: ( $id berhasil dipulihkan", 200);
-    }
 }
