e5ae43703416bb463ec4e39a08522e475d13f248
nizar-art <147240244+nizar-art@users.noreply.github.com>
2024-05-20 17:40:58 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -10,20 +12,24 @@ use Firebase\JWT\JWT;
 
 class UserController extends Controller
 {
-    public function login ( Request $request){
+    public function login(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'email' =>'required|email',
+            'email' => 'required|email',
             'password' => 'required|string'
         ]);
-        if ($validator->fails()){
-            return response()->json( $validator->messages())->setStatusCode(422);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages())->setStatusCode(422);
         }
 
         $validated = $validator->validate();
-        if ( Auth::attempt($validated)){
+
+        if (Auth::attempt($validated)) {
             $user = Auth::user();
 
             $payload = [
+                'id' => $user->id,
                 'name' => $user->name,
                 'role' => $user->role,
                 'iat' => Carbon::now()->timestamp,
@@ -31,12 +37,42 @@ class UserController extends Controller
             ];
 
             $bearer = JWT::encode($payload, env('JWT_SECRET_KEY'), 'HS256');
+
             return response()->json([
-               'nama' => $user->name,
-               'role' => $user->role,
-               'bearer' => $bearer
+                'id' => $user->id,
+                'nama' => $user->name,
+                'role' => $user->role,
+                'bearer' => $bearer
             ], 200);
         }
+
         return response()->json(["Akun tidak tersedia"], 422);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages())->setStatusCode(422);
+        }
+
+        $validated = $validator->validate();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'role' => 'user' // Set a default role, adjust as necessary
+        ]);
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user
+        ], 201);
     }
 }
