89a037e8f5233b09c8d3faccdb7300b1f2d00261
nizar-art <147240244+nizar-art@users.noreply.github.com>
2024-05-20 14:24:07 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,26 +18,21 @@ use App\Http\Controllers\GoogleAuthController;
 */
 
 Route::post('login', [UserController::class,'login']);
+Route::post('register', [UserController::class,'register']);
 
-Route::prefix('auth/google')->group(function() {
-    Route::get('', [GoogleAuthController::class, 'redirect']);
+Route::prefix('oauth/register')->group(function () {
+    Route::get('',[GoogleAuthController::class, 'redirect']);
     Route::get('call-back', [GoogleAuthController::class, 'callbackGoogle']);
 });
 
 Route::middleware(['admin.jwt'])->group(function(){
-    Route::post('product', [ProductController::class,'store']);
-    Route::get('product', [ProductController::class,'showAll']);
-    Route::get('product/{id}', [ProductController::class,'showById']);
-    Route::get('product/search/name={name}', [ProductController::class,'showByName']);
-    Route::put('product/{id}', [ProductController::class,'update']);
-    Route::delete('product/{id}', [ProductController::class,'delete']);
-    Route::get('product/{id}/restore', [ProductController::class,'restore']);
+    Route::post('products', [ProductController::class,'create']);
+    Route::get('products', [ProductController::class,'Read']);
+    Route::put('products/{id}', [ProductController::class,'update']);
+    Route::delete('products/{id}', [ProductController::class,'delete']);
 
-    Route::post('category', [CategoryController::class,'store']);
-    Route::get('category', [CategoryController::class,'showAll']);
-    Route::get('category/{id}', [CategoryController::class,'showById']);
-    Route::get('category/search/name={name}', [CategoryController::class,'showByName']);
-    Route::put('category/{id}', [CategoryController::class,'update']);
-    Route::delete('category/{id}', [CategoryController::class,'delete']);
-    Route::get('category/{id}/restore', [CategoryController::class,'restore']);
+    Route::post('categories', [CategoryController::class,'create']);
+    Route::get('categories', [CategoryController::class,'Read']);
+    Route::put('categories/{id}', [CategoryController::class,'update']);
+    Route::delete('categories/{id}', [CategoryController::class,'delete']);
 });
